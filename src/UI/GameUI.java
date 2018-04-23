ec37c6c4dbfb57705416b8b843a5bbf8aa0116b7
advaHutah <advah2009@gmail.com>
2018-04-23 12:12:05 +0100

    1.1 next time : add MissileLaunch from UI to Logic

--- src/UI/GameUI.java
+++ src/UI/GameUI.java
@@ -1,5 +1,28 @@
 package UI;
 
+import MVC.GameController;
+
 public class GameUI {
+
+	public void registerListener(GameController gameController) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void showAddMissileLauncher() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void showAddMissileLauncherDestructor() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void showAddMissileDestructor() {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
