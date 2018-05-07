f3a5480fd455442291eb23903929c98406284427
Liran <liranz10z@gmail.com>
2018-05-07 16:34:01 +0300

    1.5

--- src/Logic/Missile.java
+++ src/Logic/Missile.java
@@ -1,20 +1,72 @@
 package Logic;
 
-public class Missile {
+import java.util.Calendar;
+
+public class Missile extends Thread {
 	
 	private int id;
 	private String destination;
 	private int flyTime;
 	private int launchTime;
 	private int damage;
+	private MissileLauncher theLauncher;
+
 	
-	
-	public Missile(int id, String destination, int flyTime, int launchTime, int damage) {
+	public Missile(int id, String destination, int flyTime, int launchTime, int damage,MissileLauncher theLauncher) {
 		this.id = id;
 		this.destination = destination;
 		this.flyTime = flyTime;
 		this.launchTime = launchTime;
 		this.damage = damage;
+		this.theLauncher=theLauncher;
+	}
+
+	public void launch() throws InterruptedException {
+		synchronized (this) {
+			theLauncher.addWaitingMissile(this);
+			System.out.println(Calendar.getInstance().getTimeInMillis()
+					+ " Missile #" + getId() + " is waiting to launch");
+			//gets notified by launcher
+			wait();
+		}
+
+		synchronized (theLauncher) {
+			System.out.println(Calendar.getInstance().getTimeInMillis()
+					+ " --> Missile #" + getId() + " started launch");
+	
+			theLauncher.notifyAll();
+		}
+	}
+
+	public synchronized void fly() throws InterruptedException {
+		System.out.println(Calendar.getInstance().getTimeInMillis()
+				+ " Missile #" + getId() + " starts flying for " + flyTime
+				+ "ms");
+		
+		wait(flyTime);
+		//if missile was destroyed
+		
+		//else
+		
+		
+	}
+
+
+	
+	public long getId() {
+		return id;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			launch();
+			fly();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
--- src/Logic/MissileLauncher.java
+++ src/Logic/MissileLauncher.java
@@ -1,13 +1,17 @@
 package Logic;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
 import java.util.Vector;
 
-public class MissileLauncher {
+public class MissileLauncher implements Runnable{
 	
 	private String id;
 	private boolean isHidden;
 	private Vector<Missile> missilesToLaunch;
+	private Queue<Missile> waitingMissiles = new LinkedList<Missile>();
+
 	
 	public MissileLauncher(String id, boolean isHidden) {
 		this.id = id;
@@ -21,6 +25,80 @@ public class MissileLauncher {
 		Random random =  new Random();
 		this.isHidden = random.nextBoolean();
 	}
+	
+	public void addMissile(Missile newMissile) {
+		missilesToLaunch.add(newMissile);
+		newMissile.start();
+	}
+	
+	public synchronized void addWaitingMissile(Missile missile) {
+		waitingMissiles.add(missile);
+
+		System.out.println("After adding missile #" + missile.getId()
+				+ " there are " + waitingMissiles.size()
+				+ " missiles waiting");
+
+		synchronized (/*dummyWaiter*/this) {
+			if (waitingMissiles.size() == 1) {
+				/*dummyWaiter.*/notify(); // to let know there is an missile
+										// waiting
+			}
+		}
+	}
+	
+	public synchronized void launchMissile() {
+		Missile firstMissile = waitingMissiles.poll();
+		if (firstMissile != null) {
+
+			System.out.println("MissileLauncher is notifying Missile #"
+					+ firstMissile.getId());
+			synchronized (firstMissile) {
+				firstMissile.notifyAll();
+			}
+		}
+
+	//	synchronized (this) {
+			try {
+
+				System.out.println("MissileLauncher waits that missile #"
+						+ firstMissile.getId()
+						+ " will land/be destructed");
+
+				wait(); // wait till the missile finishes
+
+				System.out.println("Missile Launcher was announced that Missile #"
+						+ firstMissile.getId() + " is landed/destructed");
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		//}
+	}
+
+	public void run() {
+		
+		//while not being destroyed () ??? 
+		System.out.println("In Missile Launcher "+ id +" ::run");
+		
+			if (!waitingMissiles.isEmpty()) {
+				launchMissile();
+			} else {
+				synchronized (this) {
+					try {
+						System.out.println("Missile Launcher has no missiles waiting");
+						wait(); // wait till there is a missile  waiting
+		
+						//gets notified
+						System.out.println("Missile Launcher was notified there is a missile waiting");
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		
+	}
+		
+	
 	
 
 }
